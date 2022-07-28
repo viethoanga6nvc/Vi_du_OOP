adc39a5be2851825d9ac7e4e14ae62052ba940a5
Hoàng N.V <viethoang.a6nvc@gmail.com>
2022-07-28 14:27:42 +0700

    Add project files.

---
+++ VD1/NhanVien.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VD1
+{
+    class NhanVien
+    {
+        private string _MaNV;
+        public string MaNV
+        {
+            get { return _MaNV; }
+            set { _MaNV = value; }
+        }
+        private string _TenNV;
+        public string TenNV
+        {
+            get { return _TenNV; }
+            set { _TenNV = value; }
+        }
+        private int _luong1gio;
+        public int Luong1gio
+        {
+            get { return _luong1gio; }
+            set { _luong1gio = value; }
+        }
+        private int _Sogiolam;
+        public int Sogiolam
+        {
+            get { return _Sogiolam; }
+            set { _Sogiolam = value; }
+        }
+        public void Nhap(string ma, string ten, int luonggio, int giolam)
+        {
+            this.MaNV = ma;
+            this.TenNV = ten;
+            this.Luong1gio = luonggio;
+            this.Sogiolam = giolam;
+        }
+        public int tinhLuong()
+        {
+            return this.Luong1gio*this.Sogiolam;
+        }
+        public string Xuat()
+        {
+            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}",
+              this.MaNV, this.TenNV, this.Luong1gio, this.Sogiolam,
+              this.tinhLuong());
+        }
+    }
+}
---
+++ VD1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VD1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NhanVien nhanvien1 = new NhanVien();
+            nhanvien1.MaNV = "M01";
+            nhanvien1.TenNV = "Nguyen Viet Hoang";
+            nhanvien1.Luong1gio = 3;
+            nhanvien1.Sogiolam = 9;
+
+            NhanVien nhanVien2 = new NhanVien();
+            nhanVien2.Nhap("M02", "B", 2, 8);
+
+            Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}",
+              nhanvien1.MaNV, nhanvien1.TenNV,
+              nhanvien1.Luong1gio, nhanvien1.Sogiolam,
+              nhanvien1.tinhLuong());
+
+            Console.WriteLine(nhanVien2.Xuat());
+            Console.ReadLine();
+        }
+    }
+}
